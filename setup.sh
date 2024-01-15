61fd153174558716e2bbc15dbc96284047b2427a
Matt Merchant <matthew.lee.merchant@gmail.com>
2024-01-15 12:01:21 -0600

    Working solution.

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-### This secript requires Docker, wget, and dos2unix
+### This script requires Docker, wget, and dos2unix
+
 
 ### Download and build Dockerfile
-wget --no-check-certificate  https://raw.githubusercontent.com/mlmerchant/freedos_bat_runner/main/Dockerfile
+wget --no-check-certificate  https://raw.githubusercontent.com/mlmerchant/freedos_bat_runner>
 docker build . -t freedos-runner
 
 
@@ -15,18 +16,18 @@ mkdir $mnt
 ### Populate with the supporting files
 # bat file to run in command.com
 touch $mnt/input.bat
-chmod o+r $mnt/input.bat
+chmod 777 $mnt/input.bat
+
 
 ### Insert Script here
-echo "echo hello from feedos" > $mnt/input.bat
+echo "echo hello from feedos! > C:\mnt\stdout" > $mnt/input.bat
 unix2dos $mnt/input.bat
 
 # stdin to start script and optionally enter more stdin.
 cat << EOF > $mnt/stdin
-c:\mnt\input.bat > c:\mnt\stdout
 exit
 EOF
-chmod o+r $mnt/stdin
+chmod 777 $mnt/stdin
 unix2dos $mnt/stdin
 
 
